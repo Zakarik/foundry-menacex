83c81955dc7604c21901b781a9557f1531b96206
Zakarik <inconnu_jedi@hotmail.com>
2025-01-31 21:18:51 +0100

    Update menacex.js
    add Def Psi to Encounter / NPC
    add PT to NPC

--- menacex.js
+++ menacex.js
@@ -2,17 +2,19 @@ const MODULE_ID = 'foundry-menacex'; // Change this ID!
 
 Hooks.once('ready', () => { 
   console.warn(CONFIG);
-  libWrapper.register(MODULE_ID, "CONFIG.Actor.sheetClasses.character['coc.CoCActorSheet'].cls.prototype._render", characterSheet2_render, "WRAPPER");
+  libWrapper.register(MODULE_ID, "CONFIG.Actor.sheetClasses.character['coc.CoCActorSheet'].cls.prototype._render", actorSheet_render, "WRAPPER");
+  libWrapper.register(MODULE_ID, "CONFIG.Actor.sheetClasses.npc['coc.CoCNpcSheet'].cls.prototype._render", actorSheet_render, "WRAPPER");
+  libWrapper.register(MODULE_ID, "CONFIG.Actor.sheetClasses.encounter['coc.CoCEncounterSheet'].cls.prototype._render", encounterSheet_render, "WRAPPER");
 });
 
-async function characterSheet2_render(wrapped, ...args) {
+async function actorSheet_render(wrapped, ...args) {
   await wrapped(...args);
   if (!this.actor.isOwner) return;
   const sectionPT = this.element[0]?.querySelector(`div.tab.stats > div.flexrow:last-of-type div.flex1:last-of-type`);
-  const sectionDEF = this.element[0]?.querySelector(`div.tab.stats > div.flexrow div.flexrow.defence`).parentElement;
+  const sectionDEF = this.element[0]?.querySelector(`div.tab.stats > div.flexrow div.flexrow.defence`);
 
-  if(!sectionPT) return;
   // POINTS DE TENSIONS
+  if(!sectionPT) return;
   //HEADER
   const headerTemplate = Handlebars.compile(`
     <div class="flexrow table-header injuries-header no-wrap">
@@ -53,6 +55,8 @@ async function characterSheet2_render(wrapped, ...args) {
   sectionPT.innerHTML += mainSecondDivHTML;
 
   // DEF PSI
+  
+  if(!sectionDEF) return;
   // BODY
   const bodyDefTemplate = Handlebars.compile(
     `<div class="flexrow no-wrap defence">
@@ -82,7 +86,46 @@ async function characterSheet2_render(wrapped, ...args) {
     bonus:bonus,
     valueLabel: game.i18n.localize("COC.ui.value")
   });
-  sectionDEF.innerHTML += mainDefPsi;
+  sectionDEF.insertAdjacentHTML('afterend', mainDefPsi);
 
 
+};
+
+async function encounterSheet_render(wrapped, ...args) {
+  await wrapped(...args);
+  if (!this.actor.isOwner) return;
+  const sectionDEF = this.element[0]?.querySelector(`div.tab.stats div.defence`);
+  
+  // DEF PSI
+  if(!sectionDEF) return;
+  // BODY
+  const bodyDefTemplate = Handlebars.compile(
+    `<div class="flexrow no-wrap defence">
+      <div class="flex6 center cell-header">
+          <span class="field-label"><i class="fas fa-shield-alt"></i> {{defShortLabel}}</span>
+      </div>
+      <div class="flex5 center cell">
+          <input class="field-value" name="system.attributes.defpsi.base" type="text" value="{{base}}" placeholder="Base" data-dtype="Number">
+      </div>
+      <div class="flex5 center cell">
+          <input class="field-value" name="system.attributes.defpsi.bonus" type="text" value="{{bonus}}" placeholder="Bonus" data-dtype="Number">
+      </div>
+      <div class="flex6 center cell readonly">
+          <input class="field-value" name="system.attributes.defpsi.value" readonly="true" type="text" value="{{value}}" placeholder="Value" data-dtype="Number">
+      </div>
+    </div>`
+  );
+  const base = 10;
+  const bonus = parseInt(this.actor.system.attributes?.defpsi?.bonus ?? 0);
+  const charisme = parseInt(this.actor.system.stats.cha.mod);
+  const atkPsi = parseInt(this.actor.system.attacks.magic.mod);
+
+  const mainDefPsi = bodyDefTemplate({
+    defShortLabel: game.i18n.localize('MENACEX.def-short'),
+    value:base+bonus+charisme+atkPsi,
+    base:base,
+    bonus:bonus,
+    valueLabel: game.i18n.localize("COC.ui.value")
+  });
+  sectionDEF.insertAdjacentHTML('afterend', mainDefPsi);
 };
